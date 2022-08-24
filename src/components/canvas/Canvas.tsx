213c5b9ef1fd9791bbd36f3eaef41b471ef33170
vincent <vincent@eyeem.com>
2022-08-24 11:55:35 +0200

    split canvas to separate component, add typing

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
@@ -6,67 +6,80 @@ const ZOOMED_WIDTH = CANVAS_WIDTH / 2;
 const ZOOMED_HEIGHT = CANVAS_HEIGHT / 2;
 
 function Canvas() {
-  const canvasRef = useRef(null);
-  const zoomedRef = useRef(null);
-  const twinCanvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const zoomedRef = useRef<HTMLCanvasElement>(null);
+  const twinCanvasRef = useRef<HTMLCanvasElement>(null);
 
   const copyCanvas = () => {
     const canvas = canvasRef.current;
-    const dataurl = canvas.toDataURL();
+    const dataurl = canvas?.toDataURL();
 
     const twinCanvas = twinCanvasRef.current;
-    const ctx = twinCanvas.getContext("2d");
-    var img = new Image();
-    img.src = dataurl;
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0);
-    };
+    const ctx = twinCanvas?.getContext("2d");
+    const img = new Image();
+
+    if (dataurl) {
+      img.src = dataurl;
+      img.onload = () => {
+        ctx?.drawImage(img, 0, 0);
+      };
+    }
   };
 
-  function drawPixel(event) {
+  function drawPixel(event: React.MouseEvent<HTMLCanvasElement>) {
     const canvas = canvasRef.current;
-    const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
+    const rect = canvas?.getBoundingClientRect();
 
-    const ctx = canvas.getContext("2d");
-    const imageData = ctx.createImageData(4, 4);
+    if (rect && canvas) {
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
 
-    // Cada pixel tiene 4 valores
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData.data[i + 0] = 190; // R
-      imageData.data[i + 1] = 0; // G
-      imageData.data[i + 2] = 210; // B
-      imageData.data[i + 3] = 255; // A
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        const imageData = ctx.createImageData(4, 4);
+
+        // Cada pixel tiene 4 valores
+        for (let i = 0; i < imageData.data.length; i += 4) {
+          imageData.data[i + 0] = 190; // R
+          imageData.data[i + 1] = 0; // G
+          imageData.data[i + 2] = 210; // B
+          imageData.data[i + 3] = 255; // A
+        }
+
+        const x = Math.floor(mouseX / 4) * 4;
+        const y = Math.floor(mouseY / 4) * 4;
+        ctx.putImageData(imageData, x, y);
+      }
     }
-
-    const x = Math.floor(mouseX / 4) * 4;
-    const y = Math.floor(mouseY / 4) * 4;
-    ctx.putImageData(imageData, x, y);
   }
 
-  const zoom = (event) => {
+  const zoom = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const zoomedCanvas = zoomedRef.current;
-    const ctx = zoomedCanvas.getContext("2d");
+    if (zoomedCanvas) {
+      const ctx = zoomedCanvas.getContext("2d");
 
-    const canvas = canvasRef.current;
-    const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-
-    ctx.clearRect(0, 0, ZOOMED_WIDTH, ZOOMED_HEIGHT);
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-    ctx.drawImage(
-      canvas,
-      Math.min(Math.max(0, mouseX - 5), CANVAS_WIDTH - 10),
-      Math.min(Math.max(0, mouseY - 5), CANVAS_HEIGHT - 10),
-      10,
-      10,
-      0,
-      0,
-      ZOOMED_WIDTH,
-      ZOOMED_HEIGHT
-    );
+      const canvas = canvasRef.current;
+      const rect = canvas?.getBoundingClientRect();
+      if (rect) {
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+        if (ctx && canvas) {
+          ctx.clearRect(0, 0, ZOOMED_WIDTH, ZOOMED_HEIGHT);
+          // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+          ctx.drawImage(
+            canvas,
+            Math.min(Math.max(0, mouseX - 5), CANVAS_WIDTH - 10),
+            Math.min(Math.max(0, mouseY - 5), CANVAS_HEIGHT - 10),
+            10,
+            10,
+            0,
+            0,
+            ZOOMED_WIDTH,
+            ZOOMED_HEIGHT
+          );
+        }
+      }
+    }
   };
 
   return (
