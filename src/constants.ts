dec8c47027d63027aae134416fd8afe4aed2166b
ginoemiliozzi <ginoemiliozzi11@gmail.com>
2022-08-25 11:28:43 +0200

    final submission button

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export enum IMAGE_DIMENSIONS {
 
 export const SQUARE_DIMENSION = 8;
 export const MAX_PIXELS_FOR_SUBMISSION = 20;
-
+export const FINAL_SUBMISSION_TIMER_MINUTES = 5;
 export const SUBMISSION_TIMEOUT_IN_SECONDS = 15;
